edbcae0306059727520caaa416109ec0609028d2
Juani <juancapuano19@gmail.com>
2026-08-27 01:29:18 -0300

    cambios

--- proyecto/src/app.module.ts
+++ proyecto/src/app.module.ts
@@ -47,7 +47,7 @@ import { BusquedasModule } from './modules/gestion-documentos/busquedas/busqueda
       // TypeOrmModule.forFeature([Entidad]) en tus módulos
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       // entities,
-      synchronize: true,
+      synchronize: false, // Nunca usar en producción, solo para desarrollo
       ssl: process.env.DB_SSL === 'true',
     }),
 
